4571f7f5e52413685e699f90e5be7ad8c6213e37
KaanDoesNothing <kaanyt123@gmail.com>
2022-11-22 15:00:59 +0100

    Improved mobile responsiveness

--- rewrite/frontend/components/navbar.vue
+++ rewrite/frontend/components/navbar.vue
@@ -2,29 +2,11 @@
 import {useGlobalStore} from "@/stores/global";
 
 const state = useGlobalStore();
-const isDashboard = ref(false);
-const isPhone = ref(false);
-
-async function init() {
-    // const res = await (await fetch("/api/session")).json();
-
-    // if(!res.error) {
-    // state.$patch({user: res});
-    // }
-}
 
 const router = useRouter();
+const route = useRoute();
 
-isDashboard.value = useRoute().path.startsWith("/dashboard")
-
-if(process.client) {
-    isPhone.value = window?.innerWidth < 1080;
-}
-
-router.afterEach((to, from) => {
-    console.log(isPhone.value);
-    isDashboard.value = to.path.startsWith("/dashboard");
-});
+const path = computed(() => route.path);
 </script>
 
 <template>
@@ -33,16 +15,12 @@ router.afterEach((to, from) => {
             <RouterLink class="btn btn-ghost normal-case text-xl" to="/">TruckersHub</RouterLink>
         </div>
         <div class="navbar-end">
-            <template v-if="isDashboard && isPhone">
-                <label for="my-drawer-3" class="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
-                </label>
-            </template>
-            <template v-if="!isDashboard && !isPhone || !isDashboard && isPhone">
-                <RouterLink class="btn" v-if="state.token" to="/dashboard/statistics">Dashboard</RouterLink>
-            </template>
+            <label for="my-drawer-3" class="btn btn-square btn-ghost md:hidden" v-if="path.startsWith('/dashboard')">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </label>
+            <RouterLink class="btn" v-if="state.token && !path.startsWith('/dashboard')" to="/dashboard/statistics">Dashboard</RouterLink>
             <template v-if="!state.token">
                 <div class="btn-group">
                     <RouterLink class="btn" to="/auth/login">Login</RouterLink>
