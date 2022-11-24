6e58843ebf6b00ea0797b731220285eaf982f182
KaanDoesNothing <kaanyt123@gmail.com>
2022-11-24 19:18:57 +0100

    Added a online vtc members counter

--- rewrite/frontend/pages/dashboard/statistics.vue
+++ rewrite/frontend/pages/dashboard/statistics.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { useGlobalStore } from "@/stores/global";
+import { API } from "@/constants";
 
 definePageMeta({
   layout: "dashboard"
 });
 
 const state = useGlobalStore();
+
+const fetchedVtc: any = await $fetch(`${API}/vtc`, {method: "POST", body: {id: state.user.truckersmp.data.vtc.name}});
+
+const onlineMembers = fetchedVtc.data.members.filter((member: any) => member.online.data).length;
 </script>
 
 <template>
@@ -13,6 +18,7 @@ const state = useGlobalStore();
         <div class="card w-80 bg-base-100 shadow-xl m-1 text-primary-content">
             <div class="card-body">
             <h2 class="card-title">Statistics</h2>
+            <p>Online VTC Members: {{onlineMembers === 0 ? "None": onlineMembers}}</p>
             <!-- <p>Deliveries: {{state.user.events?.length}}</p> -->
             </div>
         </div>
