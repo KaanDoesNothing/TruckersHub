6c9c5fc4351543e7ac45897a36a9f8b50d38ef27
KaanDoesNothing <kaanyt123@gmail.com>
2022-11-28 21:27:38 +0100

    fix

--- api/routes/map.ts
+++ api/routes/map.ts
@@ -4,5 +4,6 @@ import {render, renderFile} from "https://deno.land/x/dejs@0.10.3/mod.ts";
 export const mapRouter = new Router();
 
 mapRouter.get("/map", async (ctx) => {
-    return ctx.response.body = await renderFile("./assets/map.ejs", {});
+    const TileMapInfo = JSON.parse(await Deno.readTextFile("./assets/TileMapInfo.json"));
+    return ctx.response.body = await renderFile("./assets/map.ejs", {TileMapInfo});
 });
