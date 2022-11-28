af7fe12862447674937b2120ecf1585966e74b5a
KaanDoesNothing <kaanyt123@gmail.com>
2022-11-28 18:43:12 +0100

    hm

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,7 @@ import { mapRouter } from "./routes/map.ts";
 
 const app = new Application();
 
-app.use(oakCors({origin: "*", preflightContinue: true}));
+app.use(oakCors({origin: "*", preflightContinue: false}));
 
 app.use(UserRouter.routes()).use(UserRouter.allowedMethods());
 app.use(VTCRouter.routes()).use(VTCRouter.allowedMethods());
