78531c00f15efa15cb2678fc4fe686166539e91e
KaanDoesNothing <kaanyt123@gmail.com>
2022-11-28 21:36:08 +0100

    welp

--- api/routes/map.ts
+++ api/routes/map.ts
@@ -1,6 +1,5 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import {render, renderFile} from "https://deno.land/x/dejs@0.10.3/mod.ts";
-import { proxy } from "https://deno.land/x/oak_http_proxy@2.1.0/mod.ts";
 
 export const mapRouter = new Router();
 
@@ -8,7 +7,3 @@ mapRouter.get("/map", async (ctx) => {
     const TileMapInfo = JSON.parse(await Deno.readTextFile("./assets/TileMapInfo.json"));
     return ctx.response.body = await renderFile("./assets/map.ejs", {TileMapInfo});
 });
-
-mapRouter.get("/Tiles", (ctx) => {
-    proxy((ctx) => new URL("http://185.205.246.23:8006/truckershub/map/Tiles"))
-})
