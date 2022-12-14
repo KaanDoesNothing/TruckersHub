f55285a85efaa3b6b868a6ff3232af2485497e47
KaanDoesNothing <kaanyt123@gmail.com>
2022-12-14 23:01:47 +0100

    Cleaning up more

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -14,38 +14,5 @@
                 </div>
             </div>
         </div>
-
-            <!-- <div class="mt-16">
-                <div class="text-center">
-                    <label class="text-2xl">Top VTCs</label>
-                </div>
-
-                <div>
-
-                </div>
-            </div> -->
     </div>
-    <!-- <div class="flex justify-center p-20"> -->
-        <!-- <div class="p-10">
-            <img src="@/assets/img/truck.png" class="w-64 ring rounded-full p-10 bg-gray-100" style="transform: translate(-50%, -50%);"/>
-        </div> -->
-    <!-- </div> -->
 </template>
-
-<script setup lang="ts">
-
-    
-    // const fetchedList: any = await $fetch(`${config.public.API}/vtc/list`);
-
-    // console.log(fetchedList.data.list);
-
-    // const vtcs = await Promise.all(fetchedList.data.list.map(async (vtc: any) => await $fetch(`${config.public.API}/vtc`, {method: "POST", body: {name: vtc.name}})));
-
-    // const sortedVTCs = vtcs.sort()
-</script>
-
-<style>
-    body {
-        /* overflow:auto; */
-    }
-</style>
