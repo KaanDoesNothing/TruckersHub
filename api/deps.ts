e68b0b08c82b9fa80092157c2fb9eeb08b0da13c
KaanDoesNothing <kaanyt123@gmail.com>
2023-07-28 00:19:30 +0200

    Whoops

--- api/deps.ts
+++ api/deps.ts
@@ -2,4 +2,4 @@ export * as mongoose from "npm:mongoose@7.4.0";
 export type {APICompany, APICompanyMembers} from "npm:@truckersmp_official/api-types";
 export * as chalk from "npm:chalk@5.3.0";
 
-export * as tst from "npm:trucksim-telemetry";
+export type * as tst from "npm:trucksim-telemetry";
