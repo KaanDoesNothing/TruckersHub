126c2f29430109d2d791b4092b5647a2ee613abc
KaanDoesNothing <kaanyt123@gmail.com>
2023-11-27 21:14:16 +0100

    fix

--- api/lib/cache.ts
+++ api/lib/cache.ts
@@ -3,4 +3,4 @@ import {connect} from "redis/mod.ts";
 import {config} from "dotenv/mod.ts";
 
 //, password: config().REDIS_PASSWORD
-export const cacheInstance = await connect({hostname: config().REDIS_HOST, password: config().REDIS_PASSWORD, port: 6380});
+export const cacheInstance = await connect({hostname: config().REDIS_HOST, port: 6380});
